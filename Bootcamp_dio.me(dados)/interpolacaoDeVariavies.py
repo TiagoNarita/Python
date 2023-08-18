a1924feee1836aaa78e361dd59d390cc7cefff5c
Tiago Narita <132394947+TiagoNarita@users.noreply.github.com>
2023-08-18 18:01:52 -0300

    Create interpolacaoDeVariavies.py

---
+++ Bootcamp_dio.me(dados)/interpolacaoDeVariavies.py
@@ -0,0 +1,9 @@
+nome = "Tiago "
+idade = 20
+profissão = "programador"
+linguagem = "Python"
+
+print("Nome: %s Idade: %d " % (nome, idade))
+print("Nome: {} Idade: {} ".format(nome, idade))
+print("Nome: {1} Idade: {0} ".format(idade, nome))
+print("Nome: {1} Idade: {0} e {1} é lindo".format(idade, nome))
