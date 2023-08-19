eb232e91100ca604886d6cf05bc53bdec578b1fa
Tiago Narita <132394947+TiagoNarita@users.noreply.github.com>
2023-08-19 13:35:15 -0300

    Create String1.py

---
+++ Bootcamp_dio.me(dados)/String1.py
@@ -0,0 +1,20 @@
+nome = "TiaGo"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = '  Olá Mundo!    '
+
+print(texto)
+
+print(texto.strip() + ".")
+print(texto.lstrip()+ ".")
+print(texto.rstrip()+ ".")
+
+menu = "Python"
+
+print(menu.center(12))
+print(menu.center(12, "-"))
+
+print(".".join(menu))
