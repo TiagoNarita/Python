018669c493b985ae7dc1d325586cb5f84cb61f9b
Tiago Narita <132394947+TiagoNarita@users.noreply.github.com>
2023-08-22 22:54:15 -0300

    Update funcoes_listas.py

--- Bootcamp_dio.me(dados)/funcoes_listas.py
+++ Bootcamp_dio.me(dados)/funcoes_listas.py
@@ -9,3 +9,23 @@ print(lista)
 lista.clear()
 
 print(lista)
+
+print("")
+cores = ["azul", "verde", "azul", "vermelho"]
+
+print(cores.count("vermelho"))
+print(cores.count("azul"))
+
+#index para achar onde o elemento aparece pela primeira vez
+
+linguagens = ["python", "java", "c", "java", "csharp" ]
+print("Index:")
+print(linguagens.index("python"))
+print(linguagens.index("java"))
+
+#pop
+
+print(linguagens)
+linguagens.pop(2)
+print(linguagens)
+
